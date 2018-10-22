b2b2de8696a3b6ca8bf8fee687b11d7481838b3a
ub5466 <43756103+ub5466@users.noreply.github.com>
2018-10-22 11:10:15 -0500

    This is my first commit.

---
+++ Action.c
@@ -0,0 +1,13 @@
+	web_custom_request("web_custom_request",
+		"URL=www.snhu.edu",
+		"Method=GET",
+		"TargetFrame=",
+		"Resource=0",
+		"Referer=",
+		"Body=",
+		LAST);
+
+	Action()
+{
+	return 0;
+}
